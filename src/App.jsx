940701c3542d966089abbb358bea96e74c75dd90
aditya <adityaevan433@gmail.com>
2024-06-26 07:10:15 +0530

    changed some prop

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Checkout from './components/Checkout.jsx';
 import Header from './components/Header.jsx';
 import Meals from './components/Meals.jsx';
 import { CartContextProvider } from './store/CartContext.jsx';
-import { UserProgressContextProvider } from './Store/UserProgressContext.jsx';
+import { UserProgressContextProvider } from './store/UserProgressContext.jsx';
 
 function App() {
   return (
