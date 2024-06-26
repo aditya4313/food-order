88babbec54e5a89999e9eed04db4d259b0a46d30
aditya <adityaevan433@gmail.com>
2024-06-26 06:26:42 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Cart from './components/Cart.jsx';
 import Checkout from './components/Checkout.jsx';
-import Header from './components/Header.jsx';
+import Header from './components/header.jsx';
 import Meals from './components/Meals.jsx';
 import { CartContextProvider } from './store/CartContext.jsx';
 import { UserProgressContextProvider } from './Store/UserProgressContext.jsx';
